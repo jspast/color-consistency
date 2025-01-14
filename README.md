324b765aef8643a2ae3bab38cc30f3805330985e
jspast <140563347+jspast@users.noreply.github.com>
2025-01-14 15:01:44 -0300

    Add paper and README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# Color Consistency
+
+MATLAB R2014a implementation of the paper [Optimizing Color Consistency in Photo Collections](https://pages.cs.huji.ac.il/danix-lab/cglab/projects/color-consistency/)
+
+Paper in portuguese detailing the implementation [here](./paper.pdf)
--- src/meanStdColorTransfer.m
+++ src/meanStdColorTransfer.m
@@ -5,7 +5,7 @@ function [ResultImage] = meanStdColorTransfer(SourceImage, SourceMean, SourceStd
     ResultImage = zeros(ImageSize);
 
     for i = 1:ImageSize(3);
-        ResultImage(:,:,i) = (double(SourceImage(:,:,i)) - SourceMean(i)) .* ((ReferenceStd(i) ./ SourceStd(i))) + ReferenceMean(i);
+        ResultImage(:,:,i) = (double(SourceImage(:,:,i)) - SourceMean(i)) .* (ReferenceStd(i) ./ SourceStd(i)) + ReferenceMean(i);
     end
 end
 
